5a42aea4067e4238c77a4a45b4546df210059838
Vishvadeep Bhatia <21vishwadeepbhatia@gmail.com>
2025-02-06 20:12:57 +0530

    finished practice questions of read map week1

--- Questions/4.py
+++ Questions/4.py
@@ -1,15 +1,13 @@
 import csv
 
-l=[
-    ["company name","price","earnings per share","book value"],
-    ["reliance",1467,66,635],
-    ["tata steel",391,89,572]
-]
+l=[["company name","price","earnings per share","book value"],
+   ["reliance",1467,66,635],
+   ["tata steel",391,89,572]
+   ]
 
 path="stocks.csv"
 
 with open(path,"w") as file:
     write=csv.writer(file)
-    for i in l:
-        write.writerow(i)
+    write.writerows(l)
     print("added successfully!!!")
