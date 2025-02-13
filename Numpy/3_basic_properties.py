15b8247822c1400ba54b59b0ddf32df38595b804
Vishvadeep Bhatia <21vishwadeepbhatia@gmail.com>
2025-02-13 21:10:42 +0530

    numpy start

---
+++ Numpy/3_basic_properties.py
@@ -0,0 +1,16 @@
+import numpy as np
+a=np.array([[1,2],[3,4]])
+b=np.array([[5,6],[7,8]])
+ 
+# itemsize
+print(f"The size of elements in 'a' is {a.itemsize} bytes ")
+
+# dtype
+print(f"the datatype of array is {a.dtype}")
+
+# size
+print(f"number of elements [present in the array is {a.size}]")
+
+# shape
+print(f"the no. of rows and columns in the array is {a.shape}")
+
